d7d14210fab7194d0d2832590c308123218ac1a8
NesterovNick <nekit.vlz@mail.ru>
2025-11-22 19:05:40 +0300

    Сменил архитектуру, добавил класс, добавил сохранение в файл и автосохранение

---
+++ src/Expence.java
@@ -0,0 +1,9 @@
+public class Expence {
+    public double amount;
+    public String category;
+
+    public Expence(double amount, String category){
+        this.amount = amount;
+        this.category = category;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
         double total = 0;
-        String removecategory = null;
-        HashMap<String, Double> catgorysum = new HashMap<>();
+        ArrayList<Expence> expences = new ArrayList<>();
         boolean count = true;
         while (count == true) {
             try {
@@ -17,7 +19,7 @@ public class Main {
             System.out.println("2. Сумма расходов");
             System.out.println("3. Выход");
             System.out.println("4. Сводка по категориям: ");
-            System.out.println("5. Удаление последней категории: ");
+            System.out.println("5. Сохранить в файл: ");
             num = sc.nextInt();
                 if (num == 1) {
                     System.out.println("Сколько ты потратил?");
@@ -25,30 +27,84 @@ public class Main {
                     System.out.println("Что за категория?");
                     sc.nextLine();
                     String category = sc.nextLine();
-                    removecategory = category;
-                    if (sum < 0) {
-                        System.out.println("Не может быть трата меньше 0");
-                    } else if (catgorysum.containsKey(category)) {
-                        sum += catgorysum.get(category);
-                        catgorysum.replace(category, sum);
-                    } else {
-                        catgorysum.put(category, sum);
-                        System.out.println("Запомнил! Ты потратил " + sum + " рублей на категорию " + category);
-
-                    }
+                    Expence newExpence = new Expence(sum, category);
+                    expences.add(newExpence);
+                    System.out.println("Запомнил! Ты потратил " + sum + " рублей на категорию " + category);
                 } else if (num == 2) {
                     total = 0;
-                    for (Double value : catgorysum.values())
-                        total += value;
+                    for (Expence exp : expences)
+                        total += exp.amount;
                     System.out.println("Вся сумма расходов: " + total);
                 } else if (num == 3) {
-                    count = false;
+                    System.out.println("Хотите сохранить файл? Да/Нет ");
+                    sc.nextLine();
+                    String answer = sc.nextLine();
+                    if (answer.equals("да") || answer.equals("Да") || answer.equals("ДА")){
+                        System.out.println("Происходит сохранение в файл.");
+                        try (FileWriter writer = new FileWriter("lastreport.txt")) {
+                            HashMap<String, Double> stats = new HashMap<>();
+                            for (Expence exp :expences) {
+                                String catgory = exp.category;
+                                double sum = exp.amount;
+                                if (stats.containsKey(catgory)) {
+                                    stats.put(catgory, stats.get(catgory) + sum);
+                                } else {
+                                    stats.put(catgory, sum);
+                                }
+                            }
+                            writer.write("ОТЧЕТ ПО КАТЕГОРИЯМ\n");
+                            writer.write("===================\n");
+                            for (String category : stats.keySet()){
+                                writer.write("- " + category + ": " + stats.get(category) + " руб.\n");
+                            }
+                            writer.write("\nОбщая сумма: " + total + " руб.\n");
+                            System.out.println("Данные успешно записаны в файл: lastreport.txt");
+                        }   catch (IOException e){
+                            System.err.println("Произошла ошибка при записи в файл: lastreport.txt");
+                        }
+                        count = false;
+
+                    } else {
+                        count = false;
+                    }
                 } else if (num == 4) {
-                    System.out.println("Статистика по категориям: ");
-                    System.out.println(catgorysum);
+                    HashMap<String, Double> stats = new HashMap<>();
+                    for (Expence exp :expences) {
+                        String catgory = exp.category;
+                        double sum = exp.amount;
+                        if (stats.containsKey(catgory)){
+                            stats.put(catgory, stats.get(catgory) + sum);
+                        } else {
+                            stats.put(catgory, sum);
+                        }
+                    }
+                    System.out.println("Статистика по категориям:");
+                    for (String category : stats.keySet()){
+                        System.out.println("- " + category + ": " + stats.get(category) + " руб.");
+                    }
                 } else if (num == 5) {
-                    catgorysum.remove(removecategory);
-                    System.out.println("Категория " + removecategory + " удалена!");
+                    System.out.println("Происходит сохранение в файл.");
+                    try (FileWriter writer = new FileWriter("report.txt")) {
+                        HashMap<String, Double> stats = new HashMap<>();
+                        for (Expence exp :expences) {
+                            String catgory = exp.category;
+                            double sum = exp.amount;
+                            if (stats.containsKey(catgory)) {
+                                stats.put(catgory, stats.get(catgory) + sum);
+                            } else {
+                                stats.put(catgory, sum);
+                            }
+                        }
+                        writer.write("ОТЧЕТ ПО КАТЕГОРИЯМ\n");
+                        writer.write("===================\n");
+                        for (String category : stats.keySet()){
+                            writer.write("- " + category + ": " + stats.get(category) + " руб.\n");
+                        }
+                        writer.write("\nОбщая сумма: " + total + " руб.\n");
+                        System.out.println("Данные успешно записаны в файл: report.txt");
+                    }   catch (IOException e){
+                        System.err.println("Произошла ошибка при записи в файл: report.txt");
+                    }
                 } else {
                     System.out.println("Такой цифры нет в меню.");
                 }
@@ -59,10 +115,3 @@ public class Main {
         }
     }
 }
-
-    /*public class Expense {
-        double amount;
-        String category;
-    }*/
-
-
